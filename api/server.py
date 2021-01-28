00bea61da68720c7b5b7b3a8f402986188614140
Martin Norling <norlingjr@gmail.com>
2021-01-28 13:31:59 +0100

    Make workers register with server on startup

--- api/server.py
+++ api/server.py
@@ -4,13 +4,28 @@ Simple web server that allows sending jobs and listing status of the worker
 containers.
 """
 
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
 
 import requests
 
 APP = Flask(__name__)
 APP.template_folder = 'templates'
+APP.workers = []
 
+@APP.route('/register', methods=['POST'])
+def register():
+    """
+    Allows worker nodes to register as available to the server.
+    This is done by posting a form with json data as:
+      {worker: <id>}
+    """
+    worker = request.json
+    if worker['worker'] not in APP.workers:
+        #pylint: disable=no-member
+        APP.logger.info("worker %s registered", worker['worker'])
+        APP.workers += [worker['worker']]
+
+    return jsonify(msg="Thanks!")
 
 @APP.route('/submit', methods=['POST'])
 def submit():
@@ -19,11 +34,17 @@ def submit():
     once the work is done.
     """
     job_data = request.json
-    #pylint: disable=no-member
-    APP.logger.info("submitting job: %s", job_data)
-    response = requests.post('http://worker:5000', data=job_data)
+    # find an idle worker
+    if len(APP.workers) == 0:
+        return jsonify(status='fail', result='no workers')
+    for worker in APP.workers:
+        status = requests.get(f'http://{worker}:5000/status')
+        if status.text != 'idle':
+            continue
+        response = requests.post(f'http://{worker}:5000', json=job_data)
+        return response.json()
 
-    return response.json()
+    return jsonify(status='fail', result='no idle workers')
 
 @APP.route('/')
 def main():
--- worker/worker.py
+++ worker/worker.py
@@ -3,19 +3,39 @@
 Simple web server that accepts POST requests with work to perform.
 """
 
+import os
 import time
 import random
 from flask import Flask, jsonify
 
+import requests
+
+# Start the server
+
 APP = Flask(__name__)
+APP.hostname = os.getenv('HOSTNAME')
 APP.status = 'idle'
+APP.server = "server"
+
+def register(server):
+    """
+    Sends a registration message to the server, to let it know that there's a
+    new worker available.
+    """
+    msg = {'worker': APP.hostname}
+    response = None
+    while not response or response.status != 200:
+        response = requests.post(f'http://{server}:5000/register', json=msg)
+        time.sleep(1)
+
+register(APP.server)
 
 @APP.route('/status')
 def status():
     """
     Returns the current status of the worker
     """
-    return jsonify(status=APP.status)
+    return APP.status
 
 
 @APP.route('/', methods=['POST'])
